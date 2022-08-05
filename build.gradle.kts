900dd79458344cd3b695759d6a7693811c8c0baf
Blatzar <46196380+Blatzar@users.noreply.github.com>
2022-08-06 00:48:04 +0200

    updated gradle and kotlin in hopes of stuff working better

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version "1.5.21"
+    kotlin("jvm") version "1.7.10"
     id("java-gradle-plugin")
     id("maven-publish")
 }
@@ -23,7 +23,7 @@ dependencies {
 
     compileOnly("com.google.guava:guava:30.1.1-jre")
     compileOnly("com.android.tools:sdk-common:30.0.0")
-    compileOnly("com.android.tools.build:gradle:7.0.0")
+    compileOnly("com.android.tools.build:gradle:7.2.2")
 
     implementation("com.github.Aliucord.dex2jar:dex-translator:d5a5efb06c")
     implementation("com.github.Aliucord.jadx:jadx-core:1a213e978d")
