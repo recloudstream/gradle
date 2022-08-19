de10dccb388ff77860836bdd0ea730aad2227103
C10udburst <18114966+C10udburst@users.noreply.github.com>
2022-08-19 15:07:54 +0200

    fix that race condition

--- src/main/kotlin/com/lagradost/cloudstream3/gradle/tasks/MakePluginsJsonTask.kt
+++ src/main/kotlin/com/lagradost/cloudstream3/gradle/tasks/MakePluginsJsonTask.kt
@@ -10,6 +10,7 @@ import org.gradle.api.file.RegularFileProperty
 import org.gradle.api.tasks.OutputFile
 import org.gradle.api.tasks.TaskAction
 import java.util.LinkedList
+import java.lang.Thread
 
 abstract class MakePluginsJsonTask : DefaultTask() {
     @get:OutputFile
@@ -20,7 +21,19 @@ abstract class MakePluginsJsonTask : DefaultTask() {
         val lst = LinkedList<PluginEntry>()
 
         for (subproject in project.allprojects) {
-            subproject.extensions.findCloudstream() ?: continue
+            val cloudstream = subproject.extensions.findCloudstream() ?: continue
+
+            /*
+                bruh why does gradle ignore task order
+                forcing me to do jank like this
+            */
+            var timeout = 10000 // 10 s timeout
+            while (cloudstream.fileSize == null) {
+                Thread.sleep(100)
+                timeout -= 100
+                if (timeout <= 0)
+                    throw RuntimeException("Timeout while fetching fileSize for ${subproject.name}")
+            }
 
             lst.add(subproject.makePluginEntry())
         }
